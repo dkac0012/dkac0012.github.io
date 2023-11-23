d2f18042943ebf83b2f37a7875f65060e3626cf4
dkac0012 <127733525+dkac0012@users.noreply.github.com>
2023-11-23 12:59:11 +0900

    Add files via upload

---
+++ 프로젝트/Book.java
@@ -0,0 +1,25 @@
+package org.javaro.lecture;
+
+public class Book {
+	private String isbn; private String title; private String author; private Student student;
+	public Book() {}
+	public Book(String isbn,String str) { this.isbn=isbn; this.title=str; this.author="unknown author";}
+	public void setIsbn(String isbn) { this.isbn =isbn;}
+	public String getIsbn() {return this.isbn;}
+	public void setTitle(String title) {this.title=title;}
+	public String getTitle() {return this.title;}
+	public void setAuthor(String author) {this.author=author;}
+	public String getAuthor() {return this.author;}
+	public void setStudent(Student sutdent) {this.student=student;}
+	public Student getStudent() {return this.student;}
+	public String toString() {
+		String available;
+		if(this.getStudent()==null) {
+			available="대출가능";
+		}else {
+			available="대출자="+this.getStudent().getName();
+		}
+		return "ISBN="+this.getIsbn()+",제목="+this.getTitle()+",저자="+this.getAuthor()+","+available;
+	}
+
+}
---
+++ 프로젝트/Student.java
@@ -0,0 +1,26 @@
+package org.javaro.lecture;
+
+public class Student {
+	private String name;
+	private String studNumber;
+	private int maxBooks;
+	public Student() {}
+	public Student(String studNumber) {this.studNumber=studNumber; this.name ="unknown name"; this.maxBooks=3;}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name =name;
+	}
+	public String getStudNumber() {
+		return this.studNumber;
+	}
+	public void setStudNumber(String studNumber) {
+		this.studNumber = studNumber;
+	}
+	public int getMaxBooks() {return maxBooks;}
+	public void setMaxBooks(int maxBooks) {this.maxBooks=maxBooks;}
+	public String toString() {
+		return "학번="+this.getStudNumber()+",학생이름="+this.getName()+"(최대"+this.maxBooks+" 권)";
+	}
+}
